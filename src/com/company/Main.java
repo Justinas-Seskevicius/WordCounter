0d033f7015eb0ab71e293f6b6370b925bee85bc8
Justinas Šeškevičius <justinas.seskevicius@tokenmill.lt>
2019-09-08 23:17:54 +0300

    fix: word counter should take input folder path from the first command line argument

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        WordCounter recorder = new WordCounter("input");
+        WordCounter recorder = new WordCounter(args[0]);
         recorder.countWords();
     }
 }
